f6b6d79fb17172ed0d1f512dff5deeab20403d66
Michael Whatcott <mdwhatcott@gmail.com>
2017-09-09 22:52:12 -0600

    Extracted a type to manage state.

--- xml.go
+++ xml.go
@@ -7,41 +7,6 @@ import (
 	"log"
 )
 
-func FormatXML(data []byte) string {
-	formatted, err := FormatXMLSafe(data)
-	if err != nil {
-		log.Panic(err)
-	}
-	return formatted
-}
-
-// https://stackoverflow.com/a/27141132/605022
-func FormatXMLSafe(data []byte) (string, error) {
-	if len(data) > 3 && DumpFMT(data[:3]) == DumpFMT(utf8ByteOrderMark) {
-		data = data[3:]
-	}
-	var buffer bytes.Buffer
-	decoder := xml.NewDecoder(bytes.NewReader(data))
-	encoder := xml.NewEncoder(&buffer)
-	encoder.Indent("", "  ")
-	for {
-		token, err := decoder.Token()
-		if err == io.EOF {
-			encoder.Flush()
-			return buffer.String(), nil
-		}
-		if err != nil {
-			return "", err
-		}
-		err = encoder.EncodeToken(token)
-		if err != nil {
-			return "", err
-		}
-	}
-}
-
-var utf8ByteOrderMark = []byte{239, 187, 191}
-
 func DumpXML(v interface{}) string {
 	dump, err := DumpXMLSafe(v)
 	if err != nil {
@@ -58,3 +23,68 @@ func DumpXMLSafe(v interface{}) (string, error) {
 
 	return string(dump), nil
 }
+
+func FormatXML(data []byte) string {
+	formatted, err := FormatXMLSafe(data)
+	if err != nil {
+		log.Panic(err)
+	}
+	return formatted
+}
+
+func FormatXMLSafe(data []byte) (string, error) {
+	return NewXMLFormatter(data).Format()
+}
+
+type XMLFormatter struct {
+	buffer  *bytes.Buffer
+	encoder *xml.Encoder
+	decoder *xml.Decoder
+}
+
+func NewXMLFormatter(data []byte) *XMLFormatter {
+	buffer := new(bytes.Buffer)
+	return &XMLFormatter{
+		buffer:  buffer,
+		encoder: setupEncoder(buffer),
+		decoder: setupDecoder(data),
+	}
+}
+
+// https://stackoverflow.com/a/27141132/605022
+func (this *XMLFormatter) Format() (string, error) {
+	for {
+		token, err := this.decoder.Token()
+		if err == io.EOF {
+			return this.finalResult()
+		} else if err != nil {
+			return "", err
+		}
+
+		err = this.encoder.EncodeToken(token)
+		if err != nil {
+			return "", err
+		}
+	}
+}
+
+func (this *XMLFormatter) finalResult() (string, error) {
+	this.encoder.Flush()
+	return this.buffer.String(), nil
+}
+
+func setupDecoder(data []byte) *xml.Decoder {
+	return xml.NewDecoder(bytes.NewReader(trimByteOrderMark(data)))
+}
+
+func setupEncoder(buffer io.Writer) *xml.Encoder {
+	encoder := xml.NewEncoder(buffer)
+	encoder.Indent("", "  ")
+	return encoder
+}
+
+func trimByteOrderMark(data []byte) []byte {
+	return bytes.TrimLeft(data, string(utf8ByteOrderMark))
+}
+
+var utf8ByteOrderMark = []byte{239, 187, 191}
